c8816b39258d2d8969e5a1b993bfd073e69d9c34
aaoratos <aaoratos@pm.me>
2023-11-09 02:39:43 +0300

    Fixed validator not dealing properly with the corner cases.
    For example, a vertical ship ending on the last row, or a horizontal ship ending on the last column, etc.

--- test_validator.py
+++ test_validator.py
@@ -1,10 +1,11 @@
-import pytest
-
-from .validator import validate_battlefield
 from ._test_data import *
+from .validator import validate_battlefield
 
 
 def test_validate_battlefield() -> None:
     assert validate_battlefield(VALID_FIELD) == True
     assert validate_battlefield(INVALID_FIELD_WITH_OVERLAP) == False
     assert validate_battlefield(INVALID_FIELD_WRONG_SHIPS_NUMBER) == False
+    assert validate_battlefield(VALID_FIELD_RANDOM_85) == True
+    assert validate_battlefield(VALID_FIELD_RANDOM_49) == True
+    assert validate_battlefield(VALID_FIELD_RANDOM_111) == True

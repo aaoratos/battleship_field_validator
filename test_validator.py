40af10d86f9277db38770e5a7f0e7eed7e7857aa
aaoratos <aaoratos@pm.me>
2023-11-09 04:45:06 +0300

    Refactored tests a little bit

--- test_validator.py
+++ test_validator.py
@@ -6,6 +6,8 @@ def test_validate_battlefield() -> None:
     assert validate_battlefield(VALID_FIELD) == True
     assert validate_battlefield(INVALID_FIELD_WITH_OVERLAP) == False
     assert validate_battlefield(INVALID_FIELD_WRONG_SHIPS_NUMBER) == False
-    assert validate_battlefield(VALID_FIELD_RANDOM_85) == True
-    assert validate_battlefield(VALID_FIELD_RANDOM_49) == True
-    assert validate_battlefield(VALID_FIELD_RANDOM_111) == True
+    assert validate_battlefield(INVALID_FIELD_EMPTY) == False
+    assert validate_battlefield(VALID_FIELD_RANDOM_01) == True
+    assert validate_battlefield(VALID_FIELD_RANDOM_02) == True
+    assert validate_battlefield(VALID_FIELD_RANDOM_03) == True
+    assert validate_battlefield(VALID_FIELD_RANDOM_04) == True

8ca9d848f39963a8898fc7fff3289cadbd1e6fcb
aaoratos <aaoratos@pm.me>
2023-11-15 20:35:00 +0300

    Fix asserts checks to comply to PEP

--- test_validator.py
+++ test_validator.py
@@ -3,11 +3,11 @@ from .validator import validate_battlefield
 
 
 def test_validate_battlefield() -> None:
-    assert validate_battlefield(VALID_FIELD) == True
-    assert validate_battlefield(INVALID_FIELD_WITH_OVERLAP) == False
-    assert validate_battlefield(INVALID_FIELD_WRONG_SHIPS_NUMBER) == False
-    assert validate_battlefield(INVALID_FIELD_EMPTY) == False
-    assert validate_battlefield(VALID_FIELD_RANDOM_01) == True
-    assert validate_battlefield(VALID_FIELD_RANDOM_02) == True
-    assert validate_battlefield(VALID_FIELD_RANDOM_03) == True
-    assert validate_battlefield(VALID_FIELD_RANDOM_04) == True
+    assert validate_battlefield(VALID_FIELD) is True
+    assert validate_battlefield(INVALID_FIELD_WITH_OVERLAP) is False
+    assert validate_battlefield(INVALID_FIELD_WRONG_SHIPS_NUMBER) is False
+    assert validate_battlefield(INVALID_FIELD_EMPTY) is False
+    assert validate_battlefield(VALID_FIELD_RANDOM_01) is True
+    assert validate_battlefield(VALID_FIELD_RANDOM_02) is True
+    assert validate_battlefield(VALID_FIELD_RANDOM_03) is True
+    assert validate_battlefield(VALID_FIELD_RANDOM_04) is True
